b80426e1862f02f3f32a95da9c2d9e99777f8778
rheakhanna <31479989+rheakhanna@users.noreply.github.com>
2018-09-26 08:41:27 -0500

    Add files via upload

---
+++ complete typeface/scripts/script.js
@@ -0,0 +1,643 @@
+console.log ("hello");
+
+$(document).ready(function(){
+
+$("#a").click('click touchstart',function(){
+     $("#a").css("animation-play-state", "paused");
+     $("#b").css("animation-play-state", "running");
+     $("#a .quarter").animate ({
+          top: '950px'},'fast');
+     $('#c .quartertwo').animate ({
+          top: '10px'}, 'fast');
+     $('#c .quartertwo').animate ({
+          left: '-200px'}, 'fast');
+     $("#m .quarter").animate ({
+          left: '-300px'},'fast');
+     $("#k .quarter").animate ({
+          top: '400px'},'fast');
+     $("#n .quarter").animate ({
+          top: '-100px'},'fast');
+     $("#d .quartertwo, #d .quarterthree").animate ({
+          top: '420px'},'fast');
+     $("#d .quartertwo").animate ({
+          left: '-80px'},'fast');
+     $("#d .quartertwo").animate ({
+          top: '600px'},'fast');
+     $("#c .quarterfour").animate ({
+          top: '500px'},'fast');
+     $("#c .quarterfour").animate ({
+          left: '400px'},'fast');
+     $("#g .bottomq").animate ({
+          left: '-900px'},'fast');
+     $("#o .quarterthree").animate ({
+          left: '-600px'},'fast');
+     $("#q .quartertwo").animate ({
+          left: '40px'},'fast');
+     $("#t .quarter").animate ({
+          top: '-200px'},'fast');
+     $("#t .bar").animate ({
+          left: '400px'},'fast');
+     $("#t .bar").animate ({
+          top: '-400px'},'fast');
+     $("#z .bar").animate ({
+          left: '630px'},'fast');
+     $("#x .quartertwo").animate ({
+          top: '100px'},'fast');
+     $("#y .bottomq").animate ({
+          left: '12px'},'fast');
+     $("#two .quartertwo").animate ({
+          top: '300px'},'fast');
+     $("#two .quartertwo").animate ({
+          left: '-100px'},'fast');
+     $("#six .bottomq").animate ({
+          left: '200px'},'fast');
+
+});  
+
+
+$("#b").click('click touchstart',function(){
+     $("#a .bar").animate ({
+          left: '235px'},'fast');
+     $("#a .bar").animate ({
+          top: '230px'},'fast');
+    $("#e .quarter").animate ({
+          left: '200px'},'fast');
+    $("#e .quarter").animate ({
+          top: '400px'},'fast');
+    $("#c .quarterfour").animate ({
+     left: '50px'},'fast');
+    $("#c .quarterfour").animate ({
+          top: '220px'},'fast');
+    $("#a .quarterthree").animate ({
+          left: '50px'},'fast');
+    $("#d .quarterthree").animate ({
+          top: '300px'},'fast');
+    $("#f .bar").animate ({
+          left: '-300px'},'fast');
+    $("#f .bar").animate ({
+          top: '400px'},'fast');
+    $("#g .quarterfour").animate ({
+          left: '130px'},'fast');
+    $("#h .quarter").animate ({
+          left: '-230px'},'fast');
+    $("#g .bottomq").animate ({
+          left: '-400px'},'fast');
+    $("#i .quarter").animate ({
+          left: '0px'},'fast');
+    $("#i .quartertwo").animate ({
+          left: '0px'},'fast');
+    $("#o .quarterthree").animate ({
+          left: '-150px'},'fast');
+    $("#m .quarterfour").animate ({
+          left: '-480px'},'fast');
+    $("#b .quartertwo").animate ({
+          left: '130px'},'fast');
+    $("#d .quartertwo").animate ({
+          top: '420px'},'fast');
+    $("#p .quartertwo").animate ({
+          top: '400px'},'fast');
+    $("#v .quarter").animate ({
+          top: '-300px'},'fast');
+    $("#o .quartertwo").animate ({
+          top: '400px'},'fast');
+    $("#y .quartertwo").animate ({
+          left: '200px'},'fast');
+    $("#x .quarter").animate ({
+          left: '100px'},'fast');
+
+
+    $("#b").css("animation-play-state", "paused");
+     $("#d").css("animation-play-state", "running");
+});  
+
+
+$("#c").click('click touchstart',function(){
+     $("#b .bar").animate ({
+          left: '20px'},'fast');
+     $("#d .quartertwo").animate ({
+          top:'300px'},'fast');
+     $("#e .quartertwo").animate ({
+          left:'50px'},'fast');
+     $("#e .quartertwo").animate ({
+          top:'300px'},'fast');
+     $("#e .quarterfour").animate ({
+          top:'100px'},'fast');
+     $("#n .quarter").animate ({
+          left:'530px'},'fast');
+     $("#j .bar").animate ({
+          left:'500px'},'fast');
+     $("#j .bar").animate ({
+          top:'140px'},'fast');
+     $("#m .quarter").animate ({
+          top:'-70px'},'fast');
+     $("#i .quarter, #i .quarterthree").animate ({
+          left:'300px'},'fast');
+     $("#o .quarter, #o .quarterfour").animate ({
+          top:'150px'},'fast');
+     $("#p .quarter").animate ({
+          top:'210px'},'fast');
+     $("#h .bar").animate ({
+          top:'500px'},'fast');
+     $('#q .quartertwo').animate ({
+          left:'-40px'},'fast');
+     $('#v .quarter').animate ({
+          top:'-120px'},'fast');
+     $('#w .bar2').animate ({
+          left:'300px'},'fast');
+     $('#y .quartertwo').animate ({
+          left:'50px'},'fast');
+
+     $("#c").css("animation-play-state", "paused");
+     $("#f").css("animation-play-state", "running");
+});  
+
+
+$("#d").click('click touchstart',function(){
+     $("#a .quarter").animate ({
+          top:'220px'},'fast');
+     $("#a .quarterthree").animate ({
+          left:'130px'},'fast');
+     $("#b .quartertwo").animate ({
+          left: '50px'},'fast');
+     $("#d .quartertwo").animate ({
+          left:'50px'},'fast');
+     $("#e .quarter").animate ({
+          top:'220px'},'fast');
+     $("#e .quarter").animate ({
+          left:'130px'},'fast');
+     $("#b .bar").animate ({
+          top:'80px'},'fast');
+     $("#c .quartertwo").animate ({
+          top:'500px'},'fast');
+     $("#c .quartertwo").animate ({
+          left:'-20px'},'fast');
+     $("#c .quartertwo").animate ({
+          left:'50px'},'fast');
+     $("#c .quartertwo").animate ({
+          top:'300px'},'fast');
+     $("#u .quartertwo").animate ({
+          left:'200px'},'fast');
+     $("#i .quarter").animate ({
+          left: '150px'},'fast');
+    $("#i .quartertwo").animate ({
+          left: '130px'},'fast');
+    $("#f .bar").animate ({
+          left: '20px'},'fast');
+    $("#f .bar").animate ({
+          top: '170px'},'fast');
+    $("#n .quarter").animate ({
+          left: '300px'},'fast');
+    $("#m .quarter").animate ({
+          left: '30px'},'fast');
+    $("#m .quarter").animate ({
+          top: '200px'},'fast');
+    $("#q .quarter").animate ({
+          top: '-40px'},'fast');
+    $("#p .quarter").animate ({
+          top: '150px'},'fast');
+    $("#s .quarter").animate ({
+          left: '250px'},'fast');
+    $("#w .quarterfour").animate ({
+          left: '400px'},'fast');
+    $("#zero .quarter").animate ({
+          top: '70px'},'fast');
+    $("#eight .quarter").animate ({
+          top: '100px'},'fast');
+    $("#g .quarterfour").animate ({
+          left: '50px'},'fast');
+    $("#h .quarter").animate ({
+          left:'50px'},'fast');
+
+
+    $("#d").css("animation-play-state", "paused");
+     $("#e").css("animation-play-state", "running");
+
+}); 
+
+
+$("#e").click('click touchstart',function(){
+     $("#m .quarterfour").animate ({
+          left: '230px'},'fast');
+     $("#o .quarterthree").animate ({
+          top: '300px'},'fast');
+     $("#o .quarterthree").animate ({
+          left:'130px'},'fast');
+     $("#o .quartertwo").animate ({
+          top:'300px'},'fast');
+     $("#o .quartertwo").animate ({
+          left:'50px'},'fast');
+     $("#g .bottomq").animate ({
+          left:'50px'},'fast');
+     $("#k .quarter").animate ({
+          top:'130px'},'fast');
+     $("#n .quarter").animate ({
+          left:'50px'},'fast');
+     $("#q .quarter").animate ({
+          top: '210px'},'fast');
+     $("#r .quarter").animate ({
+          top: '50px'},'fast');
+     $("#w .bar2").animate ({
+          left: '-60px'},'fast');
+     $("#e .quartertwo").animate ({
+          top:'400px'},'fast');
+     $("#e .quartertwo").animate ({
+          left:'-50px'},'fast');
+
+     $("#e").css("animation-play-state", "paused");
+     $("#c").css("animation-play-state", "running");
+
+     
+}); 
+
+
+$("#f").click('click touchstart',function(){
+     $("#f .bartwo").animate ({
+          left:'-250px'},'fast');
+     $("#f .bartwo").animate ({
+          top:'40px'},'fast');
+     $("#f .bartwo").animate ({
+          left:'-10px'},'fast');
+     $("#f .bartwo").animate ({
+          top:'250px'},'fast');
+     $("#a .quartertwo").animate ({
+          top:'400px'},'fast');
+     $("#a .quartertwo").animate ({
+          left:'50px'},'fast');
+     $("#a .quartertwo").animate ({
+          top:'300px'},'fast');
+     $("#b .quarterfour").animate ({
+          left:'150px'},'fast');
+     $("#b .quarterfour").animate ({
+          top:'100px'},'fast');
+     $("#b .quarterfour").animate ({
+          left:'50px'},'fast');
+     $("#e .quarterfour").animate ({
+          top:'220px'},'fast');
+     $("#g .bar").animate ({
+          left:'240px'},'fast');
+     $("#g .quarter").animate ({
+          top:'100px'},'fast');
+     $("#g .quarter").animate ({
+          left:'130px'},'fast');
+     $("#o .quarter").animate ({
+          left:'230px'},'fast');
+     $("#o .quarter, #o .quarterfour").animate ({
+          top:'220px'},'fast');
+     $("#n .quarter").animate ({
+          top:'230px'},'fast');
+     $("#n .quarter").animate ({
+          left:'260px'},'fast');
+     $("#n .bar").animate ({
+          left:'550px'},'fast');
+    
+     $("#h .quarter").animate ({
+          top:'80px'},'fast');
+     $("#m .quarter").animate ({
+          top:'190px'},'fast');
+     $("#h .bar").animate ({
+          top:'80px'},'fast');
+     $("#u .quarterfour").animate ({
+          left:'-200px'},'fast');
+     $("#w .quarter").animate ({
+          top:'-270px'},'fast');
+     $("#three .quartertwo").animate ({
+          top:'400px'},'fast');
+     $("#five .bar2").animate ({
+          top:'200px'},'fast');
+     $("#six .quarter, #six .quarterthree").animate ({
+          left:'200px'},'fast');
+     $("#nine .quartertwo, #nine .quarterthree").animate ({
+          top:'400px'},'fast');
+
+     $("#f").css("animation-play-state", "paused");
+     $("#g").css("animation-play-state", "running");
+
+     
+}); 
+
+
+$("#g").click('click touchstart',function(){
+     $("#b .quarterfour").animate ({
+          top:'220px'},'fast');
+     $("#b .quarterthree").animate ({
+          left:'200px'},'fast');
+     $("#b .quarterthree").animate ({
+          top:'300px'},'fast');
+     $("#b .quarterthree").animate ({
+          left:'130px'},'fast');
+     $("#g .quarter").animate ({
+          top:'220px'},'fast');
+     $("#e .quarterthree").animate ({
+          top:'400px'},'fast');
+     $("#e .quarterthree").animate ({
+          left:'900px'},'fast');
+     $("#j .bar").animate ({
+          top:'-20px'},'fast');
+     $("#j .bar").animate ({
+          left:'145px'},'fast');
+     $("#j .bar").animate ({
+          top:'240px'},'fast');
+     $("#j .bottomq").animate ({
+          top:'250px'},'fast');
+     $("#j .bottomq").animate ({
+          left:'440px'},'fast');
+     $("#o .quartertwo").animate ({
+          top:'500px'},'fast');
+     $("#o .quartertwo").animate ({
+          left:'-200px'},'fast');
+     $("#n .quarter").animate ({
+          left:'580px'},'fast');
+     $("#n .quarter").animate ({
+          top:'230px'},'fast');
+     $("#m .quarterfour").animate ({
+          top:'200px'},'fast');
+     $("#q .quarterthree").animate ({
+          left:'40px'},'fast');
+     $("#q .quarterthree").animate ({
+          top:'450px'},'fast');
+     $("#p .quarterfour").animate ({
+          top:'450px'},'fast');
+     $("#r .quarter").animate ({
+          top:'30px'},'fast');
+     $("#s .quarterfour").animate ({
+          left:'410px'},'fast');
+     $("#v .quarter").animate ({
+          left:'180px'},'fast');
+     $("#v .quarter").animate ({
+          top:'50px'},'fast');
+     $("#d .bar").animate ({
+          left:'230px'},'fast');
+     $("#g .quartertwo").animate ({
+          left:'50px'},'fast');
+     $("#eight .equartertwo").animate ({
+          left:'50px'},'fast');
+     $("#two .bartwo").animate ({
+          top:'-200px'},'fast');
+     $("#six .bottomq").animate ({
+          left:'250px'},'fast');
+     $("#one .bartwo").animate ({
+          top:'460px'},'fast');
+     $("#e .quarterfour").animate ({
+          top:'220px'},'fast');
+
+     $("#f").css("animation-play-state", "paused");
+     $("#g").css("animation-play-state", "paused");
+     $("#h").css("animation-play-state", "running");
+
+     
+});
+
+$("#h").click('click touchstart',function(){
+
+     $("#h .quarter").animate ({
+          top:'230px'},'fast');
+     $("#h .bar").animate ({
+          left:'300px'},'fast');
+     $("#h .bar").animate ({
+          top:'580px'},'fast');
+      $("#h .bar").animate ({
+          left:'15px'},'fast');
+       $("#i .quarter, #i .quarterthree").animate ({
+          left:'150px'},'fast');
+        $("#i .quarterthree, #i .quartertwo").animate ({
+          top:'250px'},'fast');
+     $("#k .quarter").animate ({
+          left:'300px'},'fast');
+     $("#k .quarter").animate ({
+          top:'40px'},'fast');
+     $("#l .bar").animate ({
+          left:'850px'},'fast');
+     $("#l .bar").animate ({
+          top:'310px'},'fast');
+     $("#o .quarterthree").animate ({
+          left:'50px'},'fast');
+     $("#o .quartertwo").animate ({
+          left:'50px'},'fast');
+     $("#r .quarter").animate ({
+          top:'148px'},'fast');
+     $("#p .quarterfour").animate ({
+          top:'210px'},'fast');
+     $("#s .quarterfour").animate ({
+          left:'190px'},'fast');
+     $("#v .quarter").animate ({
+          left:'350px'},'fast');
+
+     $("#h").css("animation-play-state", "paused");
+     $("#j").css("animation-play-state", "running");
+     
+});
+
+
+$("#i").click('click touchstart',function(){
+     $('#i .quarter, #i .quartertwo').animate ({
+     	opacity: '1'
+     }, 'slow');
+     
+});
+
+
+$("#j").click('click touchstart',function(){
+    $("#j .bottomq").animate ({
+          left:'0px'},'fast');
+    $("#j .bottomq").animate ({
+          top:'460px'},'fast');
+    $("#i .quartertwo, #i .quarterthree").animate ({
+          top:'190px'},'fast');
+    $("#j .bar").animate ({
+          left:'120px'},'fast');
+    $("#k .bar").animate ({
+          left:'0px'},'fast');
+    $("#o .quarterthree").animate ({
+          left:'150px'},'fast');
+    $("#o .quarter").animate ({
+          left:'150px'},'fast');
+    $("#q .quarterthree").animate ({
+          top:'290px'},'fast');
+    $("#q .bottomq").animate ({
+          left:'200px'},'fast');
+    $("#z .bar").animate ({
+          top:'-450px'},'fast');
+    $("#u .quartertwo").animate ({
+          left: '50px'},'fast');
+    $("#t .quarter").animate ({
+          top:'-430px'},'fast');
+    $("#t .quarter").animate ({
+          left:'530px'},'fast');
+    $("#r .quarterfour").animate ({
+          top:'300px'},'fast');
+    $("#r .quarterfour").animate ({
+          left:'200px'},'fast');
+    $("#n .quarter").animate ({
+          top:'-30px'},'fast');
+    $("#m .quarterfour").animate ({
+          top:'30px'},'fast');
+    $("#m .bar").animate ({
+          left:'390px'},'fast');
+    $("#m .bar").animate ({
+         top:'-70px'},'fast');
+    $("#o").animate ({
+         left:'40px'},'fast');
+    $("#q .quarter").animate ({
+         top:'30px'},'fast');
+    $("#q .quarter").animate ({
+         left:'200px'},'fast');
+    $("#v .quarter").animate ({
+         top:'-120px'},'fast');
+    $("#y .quartertwo").animate ({
+         left:'85px'},'fast');
+    $("#one .bottomq").animate ({
+         left:'60px'},'fast');
+    $("#u .quarterfour").animate ({
+          left: '50px'},'fast');
+    $("#k .quarter").animate ({
+          left: '50px'},'fast');
+
+    $("#j").css("animation-play-state", "paused");
+     $("#k").css("animation-play-state", "running");
+
+     
+});
+
+$("#k").click('click touchstart',function(){
+     $("#k .quarter").animate ({
+          top: '130px'},'fast');
+     $("#i .bar").animate ({
+          left: '149px'},'fast');
+     $("#i .bar").animate ({
+          top: '240px'},'fast');
+     $("#m .quarter").animate ({
+          top: '-110px'},'fast');
+  $("#o .quarterthree").animate ({
+          left: '50px'},'fast');
+  $("#o .quartertwo").animate ({
+          left: '130px'},'fast');
+  $("#t .quarter").animate ({
+          left: '435px'},'fast');
+  $("#t .quarter").animate ({
+          top: '-330px'},'fast');
+  $("#r .quarterfour").animate ({
+          top: '210px'},'fast');
+  $("#l").animate ({
+          top: '-10px'},'fast');
+  $("#l").animate ({
+          left: '1000px'});
+     
+});
+
+$("#l").click('click touchstart',function(){
+     $('#m .bar, #m .bar2').animate ({
+     	opacity: '1'
+     }, 'slow');
+     
+});
+
+$("#m").click('click touchstart',function(){
+     $('#a .quartertwo, #b .quartertwo, #c .quartertwo, #d .quarterthree, #e .quartertwo, #g .quarterthree').hide('slow');
+     
+});
+
+$("#n").click('click touchstart',function(){
+     $('#n .bar').animate ({
+     	opacity: '1'
+     }, 'slow');
+     $('#a .quarter, #b .quarter, #c .quarterfour, #d .quarterfour, #e .quarter, #g .quarterfour').hide('slow');
+});
+
+
+$("#o").click('click touchstart',function(){
+     $('#o .quarter, #o .quartertwo').animate ({
+     	opacity: '1'
+     }, 'slow');
+     
+});
+
+
+$("#p").click('click touchstart',function(){
+     $('#p .bar').animate ({
+     	opacity: '1'
+     }, 'slow');
+
+     $('#h .bar, #i .bar, #j .bar, #l, #m .bar, #m .bar2, #n .bar, #k .bar').animate ({
+     	opacity: '0'
+     }, 'slow');
+     
+});
+
+
+$("#q").click('click touchstart',function(){
+     $('#q .bar, #q .bottomq, #p .quarter, #p .quartertwo').animate ({
+     	opacity: '1'
+     }, 'slow');
+     $('#i .quarterfour, #i .quarterthree, #i .quarter, #i .quartertwo, #j .quarter, #j .quartertwo, #j .quarterthree, #j .quarterfour'). animate ({
+     	opacity: '0'
+     }, 'slow');
+     
+});
+
+
+$("#r").click('click touchstart',function(){
+     $('#r .bar').animate ({
+     	opacity: '1'
+     }, 'slow');
+
+     $('#q .bar, #q .bottomq, #q .quarter, #q .quartertwo').animate ({
+     	opacity: '0'
+     }, 'slow');
+     
+});
+
+$("#s").click('click touchstart',function(){
+     $('#s .quarter, #s .quarterfour, #t .bar').animate ({
+     	opacity: '1'
+     }, 'slow');
+
+     $('#p .quarter, #p .quartertwo, #p .bar').animate ({
+     	opacity: '0'
+     }, 'slow');
+     
+});
+
+$("#t").click('click touchstart',function(){
+     $('#t .bartwo').animate ({
+     	opacity: '1'
+     }, 'slow');
+
+     $('#p .quarterthree').animate ({
+     	left: '-40px'
+     }, 'slow');
+     
+});
+
+
+$("#u").click('click touchstart',function(){
+     $('#q .quarterthree').animate ({
+     	left: '-40px'
+     }, 'slow');
+
+     $('#u .quartertwo').animate ({
+     	opacity: '1'
+     }, 'slow');
+
+     $('#r .bar, #s .quarter, #s .quarterfour').animate ({
+     	opacity: '0'
+     }, 'slow');
+
+     $('#v').animate ({
+     	opacity: '1'
+     }, 'slow');
+     
+});
+
+
+$("#v").click('click touchstart',function(){
+
+    	$('#t .bar, #t .bartwo, #t .quarter').animate ({
+     	opacity: '0'
+     }, 'slow');
+ });
+
+
+
+
+
+});
